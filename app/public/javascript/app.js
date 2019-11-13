83794b19b7bb8cc55cef71a21fa3ad24abaa1d53
JC Lenz <jc@miserablepop.com>
2019-11-13 11:26:17 -0800

    Added questions and submit button to survey. Updated javascript to capture user inputs from survey

---
+++ app/public/javascript/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    //Initialize Select inputs
+    $('select').formSelect();
+
+    // Activate submit button
+    $('#submitButton').on('click', function(event){
+        event.preventDefault();
+
+        // User inputs
+        var userInput = {
+            name: $('#userName').val().trim(),
+            photo: $('#userPhoto').val().trim(),
+            scores: [
+                $('#question1').val().trim(),
+                $('#question2').val().trim(),
+                $('#question3').val().trim(),
+                $('#question4').val().trim(),
+                $('#question5').val().trim(),
+                $('#question6').val().trim(),
+                $('#question7').val().trim(),
+                $('#question8').val().trim(),
+                $('#question9').val().trim(),
+                $('#question10').val().trim()
+            ]
+        };
+        console.log('userInput = ' + JSON.stringify(userInput));
+    });
+});
