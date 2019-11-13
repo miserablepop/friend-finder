682e6c2664fd5c227b70f131a01bba64e7d28c3e
JC Lenz <jc@miserablepop.com>
2019-11-13 12:26:02 -0800

    Updated friendsArray with data

---
+++ app/data/friends.js
@@ -0,0 +1,166 @@
+// Friends array to store the possible matches
+
+var friendsArray = [
+	{
+		name: 'Jim Halpert',
+		photo: 'https://vignette.wikia.nocookie.net/theoffice/images/9/9a/Jim.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Michael Scott',
+		photo: 'https://vignette.wikia.nocookie.net/theoffice/images/0/02/Michael_Scott.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Pam Beesly',
+		photo: 'https://vignette.wikia.nocookie.net/theoffice/images/6/67/Pam_Beesley.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Dwight Schrute',
+		photo: 'https://vignette.wikia.nocookie.net/theoffice/images/c/c5/Dwight_.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Andy Bernard',
+		photo: 'https://vignette.wikia.nocookie.net/theoffice/images/b/b5/Andy_Bernard.jpg/',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Ryan Howard',
+		photo: 'https://vignette.wikia.nocookie.net/theoffice/images/2/2d/Ryan_Howard.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Stanley Hudson',
+		photo: 'https://vignette.wikia.nocookie.net/theoffice/images/2/23/Stanley_Hudson.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Angela Schrute',
+		photo: 'https://vignette.wikia.nocookie.net/theoffice/images/0/0b/Angela_Martin.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Kevin Malone',
+		photo: 'https://vignette.wikia.nocookie.net/theoffice/images/b/b2/2009Kevincropped.PNG',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Creed Bratton',
+		photo: 'https://vignette.wikia.nocookie.net/theoffice/images/2/20/2009Creed.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
