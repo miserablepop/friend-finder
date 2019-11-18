a6e8d80fc708115502c64a1f38cf5e5b447b0dec
JC Lenz <jc@miserablepop.com>
2019-11-18 13:46:54 -0800

    Updated server.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,7 +4,7 @@ var path = require('path');
 module.exports = function(app){
     
     // Home page
-    app.get('/', function(req,res){
+    app.get('/', function(req, res){
         res.send('Hello World!')
     })
 }
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 // Required dependencies 
 const express = require('express');
 var path = require('path');
-var htmlRoutes = require('./app/routing/htmlRoutes.js');
 
 // Set port to 8080
 const PORT = 8080;
 
 // Configure the Express application
-const app = express(htmlRoutes);
+const app = express();
+
+// Public directory to access CSS file
+app.use(express.static(path.join(__dirname, './app/public')));
+
+// // Add application routes
+// require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+// require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
 // Start listening on port 8080
 app.listen(PORT, function(){
