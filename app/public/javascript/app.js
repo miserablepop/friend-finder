a8c5b99f5b4bee2368521ab020916a32b3d1cad4
Juan Carlos Lenz <jclenz@miserablepop.local>
2019-11-18 15:33:10 -0800

    Removing form validation testing in app.js

--- app/public/javascript/app.js
+++ app/public/javascript/app.js
@@ -3,28 +3,37 @@ $(document).ready(function(){
     //Initialize Select inputs
     $('select').formSelect();
 
+    // for HTML5 "required" attribute
+    $("select[required]").css({
+        display: "inline",
+        height: 0,
+        padding: 0,
+        width: 0
+      });
+
     // Activate submit button
     $('#submitButton').on('click', function(event){
         event.preventDefault();
 
-        // User inputs
-        var userInput = {
-            name: $('#userName').val().trim(),
-            photo: $('#userPhoto').val().trim(),
-            scores: [
-                $('#question1').val().trim(),
-                $('#question2').val().trim(),
-                $('#question3').val().trim(),
-                $('#question4').val().trim(),
-                $('#question5').val().trim(),
-                $('#question6').val().trim(),
-                $('#question7').val().trim(),
-                $('#question8').val().trim(),
-                $('#question9').val().trim(),
-                $('#question10').val().trim()
-            ]
-        };
-        console.log('userInput = ' + JSON.stringify(userInput));
+    
+        // // User inputs
+        // var userInput = {
+        //     name: $('#userName').val().trim(),
+        //     photo: $('#userPhoto').val().trim(),
+        //     scores: [
+        //         $('#question1').val().trim(),
+        //         $('#question2').val().trim(),
+        //         $('#question3').val().trim(),
+        //         $('#question4').val().trim(),
+        //         $('#question5').val().trim(),
+        //         $('#question6').val().trim(),
+        //         $('#question7').val().trim(),
+        //         $('#question8').val().trim(),
+        //         $('#question9').val().trim(),
+        //         $('#question10').val().trim()
+        //     ]
+        // };
+        // console.log('userInput = ' + JSON.stringify(userInput));
 
         
     });
