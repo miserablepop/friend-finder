75b4fe408655a9f4233374e1f7640f0dac4c4bbd
Juan Carlos Lenz <jclenz@miserablepop.local>
2019-11-19 16:02:49 -0800

    added friend matching functionality to apiRoutes.js when POST to /friends occurs. Then added the result functionality to app.js to display the match in a modal.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,9 +11,34 @@ module.exports = function(app){
         
         var newFriend = req.body;
         console.log(newFriend);
+
+        // parseInt for scores
+        for(var i = 0; i < user.scores.length; i++) {
+            user.scores[i] = parseInt(user.scores[i]);
+        }
+
+        // default friend match is the first friend but result will be whoever has the minimum difference in scores
+        var bestFriendIndex = 0;
+        var minimumDifference = 40;
+
+        // in this for-loop, start off with a zero difference and compare the user and the ith friend scores, one set at a time
+        //  whatever the difference is, add to the total difference
+        for(var i = 0; i < friends.length; i++) {
+            var totalDifference = 0;
+            for(var j = 0; j < friends[i].scores.length; j++) {
+                var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
+                totalDifference += difference;
+            }
+
+            // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
+            if(totalDifference < minimumDifference) {
+                bestFriendIndex = i;
+                minimumDifference = totalDifference;
+            }
+        }   
         
         friends.push(newFriend);
-        res.json(newFriend);
+        res.json(friends[bestFriendIndex]);
     });
 
 };
